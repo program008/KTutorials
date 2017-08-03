a5ef24a577a2dd96a3e6122a65637b78c6624f82
program008 <1937553940@qq.com>
2017-08-03 20:22:39 +0800

    Initial commit

---
+++ src/basics/BasicsType.kt
@@ -0,0 +1,23 @@
+package basics
+
+/**
+ * Created by tao.liu on 2017/8/3.
+ * kotlin 基本类型基
+ */
+fun main(args: Array<String>) {
+
+}
+
+/**
+ * 数字
+ */
+fun method01(){
+    //你可以使用下划线使数字常量更易读：
+    val oneMillion = 1_000_000
+    val creditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
+
+
+}
